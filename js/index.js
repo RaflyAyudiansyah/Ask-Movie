e5247a25a6902268e21092c2d260723233d24acb
raflyayudiansyah <anxiaa31@gmail.com>
2023-04-15 15:29:51 +0700

    add fitur random update on main page

--- js/index.js
+++ js/index.js
@@ -7,13 +7,59 @@ setInterval(function () {
   }
 }, 5000);
 
+let show = "";
+for (i = 1; i <= 12; i++) {
+  const randomArr = ["black", "white", "yellow", "harry potter", "america", "mother", "mama", "fast", "baby", "gosh"];
+  let randomArrElement = randomArr[getRandomElementIndex()];
+
+  function getRandomElementIndex() {
+    let random = Math.random() * randomArr.length;
+    return Math.floor(random);
+  }
+  console.log(randomArrElement);
+
+  const randomNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let randomNumElement = randomNum[getRandomElementIndexNum()];
+
+  function getRandomElementIndexNum() {
+    let randomN = Math.random() * randomNum.length;
+    return Math.floor(randomN);
+  }
+  console.log(randomNumElement);
+
+  $.ajax({
+    url: "http://www.omdbapi.com/?apikey=db45875a&s=" + randomArrElement,
+    success: (results) => {
+      const moviesShow = results.Search;
+      show += showCard2(moviesShow[randomNumElement]);
+      $(".movie-container").html(show);
+    },
+    error: (e) => {
+      console.log(e.responseText);
+    },
+  });
+}
+
+// tombol detail diklik
+$(".modal-button-kedua").on("click", function () {
+  $.ajax({
+    url: "http://www.omdbapi.com/?apikey=db45875a&i=" + $(this).data("imdbid"),
+    success: (m) => {
+      const movieDetails = showMovieDetail(m);
+      $(".modal-body").html(movieDetails);
+    },
+    error: (e) => {
+      console.log(e.responseText);
+    },
+  });
+});
+
 $(".search-btn").on("click", function () {
   $.ajax({
     url: "http://www.omdbapi.com/?apikey=db45875a&s=" + $(".input-key").val(),
     success: (results) => {
       const movies = results.Search;
       let cards = "";
-      console.log = "results";
       movies.forEach((m) => {
         cards += showCard(m);
       });
@@ -44,9 +90,22 @@ function showCard(m) {
                 <div class="card">
                     <img src="${m.Poster}" class="card-img-top" alt="" />
                     <div class="card-body">
-                    <h5 class="card-title">${m.Title}</h5>
+                    <h5 class="card-title">${m.Title} (${m.Year})</h5>
                     <p class="card-text text-muted">${m.Type}</p>
                     <a href="#" class="btn btn-primary modal-button" data-bs-toggle="modal" data-bs-target="#MovieButtonDetail" data-imdbid="${m.imdbID}">Show More</a>
+                    </div>
+                </div>
+            </div>`;
+}
+
+function showCard2(m) {
+  return `<div class="col-md-4 my-2">
+                <div class="card">
+                    <img src="${m.Poster}" class="card-img-top" alt="" />
+                    <div class="card-body">
+                    <h5 class="card-title">${m.Title} (${m.Year})</h5>
+                    <p class="card-text text-muted">${m.Type}</p>
+                    <a href="#" class="btn btn-primary modal-button-kedua" data-bs-toggle="modal" data-bs-target="#MovieButtonDetail" data-imdbid="${m.imdbID}">Show More</a>
                     </div>
                 </div>
             </div>`;
